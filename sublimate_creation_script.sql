d496635b3959c9b4fcff90924df5b822dac57f25
Matthew Fletcher <fletcherm6@gmail.com>
2018-06-26 14:17:50 -0400

    added a fridge table

--- sublimate_creation_script.sql
+++ sublimate_creation_script.sql
@@ -1,6 +1,13 @@
 USE sublimate;
 
 DROP TABLE IF EXISTS Items;
+DROP TABLE IF EXISTS Fridges;
+
+CREATE TABLE Fridges (
+	Id INT NOT NULL AUTO_INCREMENT,
+    MacAddr VARCHAR(255) NOT NULL,
+    PRIMARY KEY (Id)
+);
 
 CREATE TABLE Items (
 	FridgeId INT NOT NULL CHECK (FridgeId>=1),
@@ -11,34 +18,16 @@ CREATE TABLE Items (
     Entry DATE NOT NULL,
 	BestBefore DATE,
     Quantity INT NOT NULL,
-    CONSTRAINT PK_Items PRIMARY KEY (FridgeId, ShelfId, ItemId)
+    CONSTRAINT PK_Items PRIMARY KEY (FridgeId, ShelfId, ItemId),
+    FOREIGN KEY (FridgeId) REFERENCES Fridges(Id)
 );
 
 CREATE INDEX Item_Fridge ON Items (FridgeId);
 CREATE INDEX Shelf_Fridge ON Items (FridgeId, ShelfId);
 
-DROP PROCEDURE IF EXISTS GetNextFridgeId;
-DROP PROCEDURE IF EXISTS GetNextShelfId;
 DROP PROCEDURE IF EXISTS GetNextItemId;
 
 delimiter $$
-CREATE PROCEDURE GetNextFridgeId (OUT id INT)
-	BEGIN
-		SELECT (MAX(FridgeId) + 1) INTO id FROM Items;
-        IF id IS NULL THEN SET id=1;
-        END IF;
-    END$$
-    
-CREATE PROCEDURE GetNextShelfId (IN fridgeId INT, OUT id INT)
-	BEGIN
-		IF (SELECT COUNT(*) FROM Items WHERE FridgeId = fridgeId) = 0 THEN
-			SELECT -1 INTO id;
-        ELSE
-			SELECT (MAX(ShelfId) + 1) INTO id FROM Items WHERE FridgeId = fridgeId;
-			IF id IS NULL THEN SET id=1;
-			END IF;
-		END IF;
-    END$$
     
 CREATE PROCEDURE GetNextItemId(IN fridgeId INT, IN shelfId INT, OUT id INT)
 	BEGIN
@@ -52,19 +41,16 @@ CREATE PROCEDURE GetNextItemId(IN fridgeId INT, IN shelfId INT, OUT id INT)
     END$$
 delimiter ;
 
-CALL GetNextFridgeId(@0);
-CALL GetNextShelfId (0, @b);
 CALL GetNextItemId(0, 0, @c);
 
-SELECT @0, @b, @c;
+SELECT @c;
+
+INSERT INTO Fridges(MacAddr) VALUES ("THis is not a MAC ADDRESS");
 
 INSERT INTO Items (FridgeId, ShelfId, ItemId, Product, Weight, Quantity, Entry)
 	Values (1, 1, 1, "Test", 150, 2, CURDATE());
 
 
-
-CALL GetNextFridgeId(@0);
-CALL GetNextShelfId (0, @b);
 CALL GetNextItemId(0, 0, @c);
 
-SELECT @0, @b, @c;
+SELECT  @c;Item_Fridge
